7a9fcafe1487d086eaaef249506eec764d10b438
YehorYu <104828867+YehorYu@users.noreply.github.com>
2022-09-21 21:02:28 +0300

    HW 8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", OnSubmit);
+function OnSubmit(e) {
+  e.preventDefault();
+  console.dir(e.currentTarget);
+  const { email, password } = e.currentTarget.elements;
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+  e.currentTarget.reset();
+  console.log(data);
+}
